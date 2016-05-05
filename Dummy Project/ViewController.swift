08701383ba2b39ba30f96c54092412636d6aae74
Snigdhaman Chatterjee <snigdhaman1991@gmail.com>
2016-05-05 20:04:17 +0530

    dummy project

--- Dummy Project/ViewController.swift
+++ Dummy Project/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func submitButton(sender: AnyObject) {
+        displayLabel.text = "Your age is \(textField.text!)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
